85cb0c106c8ffca633245df5593d5c2e80af264e
s33b00r <qubert00@live.se>
2020-08-31 15:18:26 +0200

    First commit

---
+++ src/Window.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class Window extends JFrame{
+
+
+
+}
---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    static Window window;
+
+    public static void main(String[] args) {
+        window = new Window();
+        window.setSize(800, 800);
+        window.setVisible(true);
+    }
+}
