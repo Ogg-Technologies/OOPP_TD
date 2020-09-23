d521f7328ae4ddeed0d2305377d61f0cc49a11d7
Erik <erik.g.eriksson1@gmail.com>
2020-09-23 20:13:22 +0200

    Extensibility improvements to GUIPanel

--- src/view/layers/GUIPanel.java
+++ src/view/layers/GUIPanel.java
@@ -8,26 +8,36 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GUIPanel extends JPanel {
-    private final double hpWidth = 0.01;
+
+    private static final Color HEALTH_COLOR = Color.GREEN;
+    private static final Color DAMAGE_COLOR = Color.RED;
+    private static final double HEALTH_BAR_WIDTH = 0.01;
+
     private VectorD pos;
     private final ModelData modelData;
 
-    public GUIPanel(VectorD pos, ModelData modelData) {
-        this.pos = pos;
+    public GUIPanel(VectorD fractionPos, ModelData modelData) {
+        this.pos = fractionPos;
         this.modelData = modelData;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        drawHealthBar(g);
+    }
+
+    private void drawHealthBar(Graphics g) {
         double hp = modelData.getBaseHealth().getFraction();
         int hpMaxHeight = getHeight() - SwingView.heightOffset;
-        g.setColor(Color.red);
-        double x = (pos.x * (getWidth() - SwingView.widthOffset) - (hpWidth * getWidth()));
+        double x = (pos.x * (getWidth() - SwingView.widthOffset) - (HEALTH_BAR_WIDTH * getWidth()));
         double y = (pos.y * getHeight());
         double height = ((hpMaxHeight - y) * (1 - pos.y));
-        g.fillRect((int) x, (int) y, (int) (hpWidth * getWidth()), (int) (height) - 1);//Rounding error causes -1
-        g.setColor(Color.green);
-        g.fillRect((int) x, (int) (y + ((1 - hp) * height)), (int) (hpWidth * getWidth()), (int) ((height * hp)));
+
+        g.setColor(DAMAGE_COLOR);
+        g.fillRect((int) x, (int) y, (int) (HEALTH_BAR_WIDTH * getWidth()), (int) (height) - 1);//Rounding error causes -1
+
+        g.setColor(HEALTH_COLOR);
+        g.fillRect((int) x, (int) (y + ((1 - hp) * height)), (int) (HEALTH_BAR_WIDTH * getWidth()), (int) ((height * hp)));
     }
 }
