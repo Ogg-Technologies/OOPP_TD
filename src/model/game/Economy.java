a060284b081739fcf6e6481ccc049fb3416d6766
Sebulon <57720614+Sebulon@users.noreply.github.com>
2020-09-28 22:59:42 +0200

    Added an Economy Class

---
+++ src/model/game/Economy.java
@@ -0,0 +1,94 @@
+package model.game;
+
+
+import model.game.enemy.concreteenemies.BasicEnemy;
+import model.game.tower.Tower;
+import model.game.tower.concretetowers.GrizzlyBear;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A class that take care of all the prices and values of towers and enemies.
+ * It also stores the amount of money the player has, and will tell if the player can
+ * afford different towers.
+ */
+public class Economy {
+
+    private final Map<Class<? extends Tower>, Integer> towerMap = new HashMap<>();
+    private final Map<BasicEnemy.Type, Integer> basicEnemyMap = new HashMap<>();
+    private int money = 0;
+
+    /**
+     * Creates an instance of the economy class, while also
+     * sets up hashMaps that hold the values of the objects.
+     */
+    public Economy(){
+        setupTowerPriceMap();
+        setupBasicEnemyMap();
+    }
+
+    private void setupBasicEnemyMap() {
+        basicEnemyMap.put(BasicEnemy.Type.FISH_AND_CHIPS, 1);
+        basicEnemyMap.put(BasicEnemy.Type.FISH_IN_A_BOAT, 2);
+        basicEnemyMap.put(BasicEnemy.Type.FISHSTICK, 1);
+        basicEnemyMap.put(BasicEnemy.Type.FISH_IN_FISH_TANK, 10);
+        basicEnemyMap.put(BasicEnemy.Type.SAILFISH, 5);
+        basicEnemyMap.put(BasicEnemy.Type.SHARK, 5);
+        basicEnemyMap.put(BasicEnemy.Type.SWORDFISH, 3);
+    }
+
+    private void setupTowerPriceMap() {
+        towerMap.put(GrizzlyBear.class, 100);
+    }
+
+
+    /**
+     * @return amount of money the player has
+     */
+    public int getMoney() {
+        return money;
+    }
+
+    /**
+     * Adds money to the player
+     * @param money amount of money to add.
+     */
+    public void addMoney(int money) {
+        if(money < 0){
+            throw new IllegalArgumentException("Cannot add negative amount of money");
+        }
+        this.money += money;
+    }
+
+    /**
+     * This takes in a tower class to check if it can be bought.
+     * If the tower can be bought, money will reduce by the price of the tower.
+     * The method also returns a boolean that says if the tower has been "bought"
+     * @param towerToBuy the tower to be bought
+     * @return a boolean that says if the tower has been bought.
+     */
+    public boolean buyTower(Class<? extends Tower> towerToBuy) {
+        Integer price = towerMap.get(towerToBuy);
+        if(towerMap.get(towerToBuy) == null){
+            throw new IllegalArgumentException("This tower does not exists in economy class");
+        }
+        if(money >= price) {
+            money -= price;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * This method gives money equal to the predefined value of the parameter.
+     * @param type a BasicEnemy.Type, which value adds to the players money,
+     */
+    public void addMoney(BasicEnemy.Type type) {
+        Integer value = basicEnemyMap.get(type);
+        if(value == null){
+            throw new IllegalArgumentException("This basic enemy type is not in economy class");
+        }
+        money += value;
+    }
+}
---
+++ test/model/game/EconomyTest.java
@@ -0,0 +1,93 @@
+package model.game;
+
+import model.game.enemy.concreteenemies.BasicEnemy;
+import model.game.tower.Tower;
+import model.game.tower.concretetowers.GrizzlyBear;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EconomyTest {
+    Economy economy;
+
+    @BeforeEach
+    void setup(){
+        economy = new Economy();
+    }
+
+    @Test
+    void getMoneyFromBeginning(){
+        assertEquals(0, economy.getMoney());
+    }
+
+    @Test
+    void addMoney(){
+        economy.addMoney(10);
+    }
+
+    @Test
+    void addMoneyAndAssertThatTheMoneyIsThatMuch(){
+        economy.addMoney(10);
+        assertEquals(10, economy.getMoney());
+    }
+
+    @Test
+    void addMoneyTwice(){
+        economy.addMoney(10);
+        assertEquals(10, economy.getMoney());
+        economy.addMoney(10);
+        assertEquals(20, economy.getMoney());
+    }
+
+    @Test
+    void addNegativeMoney(){
+        assertThrows(IllegalArgumentException.class, () -> economy.addMoney(-10));
+    }
+
+    @Test
+    void canBuyTowerIfHasEnoughMoney(){
+        economy.addMoney(100);
+        assertTrue(economy.buyTower(GrizzlyBear.class));
+    }
+
+    @Test
+    void buyingTowerDecreasesAmountOfMoney(){
+        int moneyCurrent = 100;
+        economy.addMoney(moneyCurrent);
+        economy.buyTower(GrizzlyBear.class);
+        assertTrue(moneyCurrent > economy.getMoney());
+    }
+
+    @Test
+    void cannotHaveNegativeMoney(){
+        economy.addMoney(1);
+        economy.buyTower(GrizzlyBear.class);
+        assertTrue(economy.getMoney() >= 0);
+    }
+
+    @Test
+    void cannotBuyTowersIfMoneyDoesNotDecrease(){
+        int lastMoney = economy.getMoney();
+        assertFalse(economy.buyTower(GrizzlyBear.class));
+        assertEquals(lastMoney, economy.getMoney());
+    }
+
+    @Test
+    void cannotBuyNotPricedTowers(){
+        assertThrows(IllegalArgumentException.class, () -> economy.buyTower(Tower.class));
+    }
+
+    @Test
+    void canGetMoreMoneyIfBasicEnemyIsInArgument(){
+        int curMoney = economy.getMoney();
+        economy.addMoney(BasicEnemy.Type.FISH_AND_CHIPS);
+        assertTrue(curMoney < economy.getMoney());
+    }
+
+    @Test
+    void canOnlyTakeInKnownTypesOfBasicEnemy(){
+        assertThrows(IllegalArgumentException.class, () -> economy.addMoney(null));
+    }
+
+}
