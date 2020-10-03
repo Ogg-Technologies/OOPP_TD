31b6c33e5637ba925566327c09c0069811a16fbd
ApansKaffekopp <55796574+ApansKaffekopp@users.noreply.github.com>
2020-10-03 19:16:02 +0200

    Added TowerPanel part of GUI
    Co-Authored-By: Sebulon <57720614+Sebulon@users.noreply.github.com>

---
+++ src/controller/ControllerState.java
@@ -0,0 +1,12 @@
+package controller;
+
+import model.game.tower.Tower;
+
+/**
+ * @author Sebastian, Samuel
+ * The state of the controller.
+ * Is used by controller.
+ */
+public class ControllerState {
+    static Class<?extends Tower> selectedTower;
+}
--- src/view/ButtonClickHandler.java
+++ src/view/ButtonClickHandler.java
@@ -1,5 +1,6 @@
 package view;
 
+import model.game.tower.Tower;
 import utils.Vector;
 
 /**
@@ -9,9 +10,16 @@ import utils.Vector;
 public interface ButtonClickHandler {
     /**
      * Adds a button without an argument
-     * @param percentStartPos startPos as a percent of window size
-     * @param percentSize size of button as a percent of window size
+     *
+     * @param percentStartPos        startPos as a percent of window size
+     * @param percentSize            size of button as a percent of window size
      * @param onClickWithoutArgument what will happen when button is clicked on
      */
     void addButtonWithoutArgument(Vector percentStartPos, Vector percentSize, OnClickWithoutArgument onClickWithoutArgument);
+
+    /**
+     * Sets the selected tower as the tower class from the corresponding button.
+     * @param towerClass the tower to be selected.
+     */
+    void setSelectedTower(Class<? extends Tower> towerClass);
 }
