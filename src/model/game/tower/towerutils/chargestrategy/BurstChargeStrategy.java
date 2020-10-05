38b1df7b4134ba6c6dcd01055bba266fdb307ec9
Behrozrazaq <Behroz.razaq@gmail.com>
2020-10-05 23:52:39 +0200

    Added BurstChargeStrategy

---
+++ src/model/game/tower/towerutils/chargestrategy/BurstChargeStrategy.java
@@ -0,0 +1,45 @@
+package model.game.tower.towerutils.chargestrategy;
+
+/**
+ * @author Behroz
+ * Charge strategy whith reguler bursts of attack and the pauses for set ammount of ticks
+ */
+public class BurstChargeStrategy implements ChargeStrategy {
+    private final int maxCharge;
+    private final int attacksPerCharge;
+    private int attacksLeft;
+
+    private int charge = 0;
+
+    /**
+     * @param maxCharge        the number of updates between bursts
+     * @param attacksPerCharge the number of attacks in one burst
+     */
+    public BurstChargeStrategy(int maxCharge, int attacksPerCharge) {
+        this.maxCharge = maxCharge;
+        this.attacksPerCharge = attacksPerCharge;
+        this.attacksLeft = attacksPerCharge;
+    }
+
+    @Override
+    public void update() {
+        if (charge < maxCharge) {
+            charge++;
+        }
+    }
+
+    @Override
+    public boolean isReadyToFire() {
+        return charge == maxCharge;
+    }
+
+    @Override
+    public void didFire() {
+        attacksLeft--;
+        if (attacksPerCharge == 0) {
+            attacksLeft = attacksPerCharge;
+            charge = 0;
+        }
+    }
+}
+
