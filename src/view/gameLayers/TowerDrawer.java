f4d3232c26f7ddf507e9c099f2e159d3477cec63
Sebulon <57720614+Sebulon@users.noreply.github.com>
2020-10-13 11:25:37 +0200

    Refactored TowerDrawer some more

--- src/view/gameLayers/TowerDrawer.java
+++ src/view/gameLayers/TowerDrawer.java
@@ -35,50 +35,38 @@ public class TowerDrawer implements TowerVisitor {
 
     @Override
     public void visit(GrizzlyBear tower) { //TODO Possible more refactoring by adding a getAngle to tower interface
-
-        Vector pos = getRealPos(tower.getPos());
-        BufferedImage rotatedImage = ImageHandler.getImage(Constant.getInstance().IMAGE_PATH.GRIZZLY_BEAR, tower.getAngle());
-        drawTower(rotatedImage, pos, tower.getAngle());
+        drawTower(tower.getPos(), tower.getAngle(), Constant.getInstance().IMAGE_PATH.GRIZZLY_BEAR);
     }
 
     @Override
     public void visit(BearryPotter tower) {
-
-        Vector pos = getRealPos(tower.getPos());
-        BufferedImage rotatedImage = ImageHandler.getImage(Constant.getInstance().IMAGE_PATH.BEARRY_POTTER, tower.getAngle());
-        drawTower(rotatedImage, pos, tower.getAngle());
+        drawTower(tower.getPos(), tower.getAngle(), Constant.getInstance().IMAGE_PATH.BEARRY_POTTER);
     }
 
     @Override
     public void visit(SniperBear tower) {
-
-        Vector pos = getRealPos(tower.getPos());
-        BufferedImage rotatedImage = ImageHandler.getImage(Constant.getInstance().IMAGE_PATH.SNIPER_BEAR, tower.getAngle());
-        drawTower(rotatedImage, pos, tower.getAngle());
+        drawTower(tower.getPos(), tower.getAngle(), Constant.getInstance().IMAGE_PATH.SNIPER_BEAR);
     }
 
     @Override
     public void visit(SovietBear tower) {
-
-        Vector pos = getRealPos(tower.getPos());
-        BufferedImage rotatedImage = ImageHandler.getImage("resource/sovietBear.png", tower.getAngle());
-        drawTower(rotatedImage, pos, tower.getAngle());
+        drawTower(tower.getPos(), tower.getAngle(), Constant.getInstance().IMAGE_PATH.SOVIET_BEAR);
     }
 
     @Override
     public void visit(Barbearian tower) {
-        Vector pos = getRealPos(tower.getPos());
-        BufferedImage rotatedImage = ImageHandler.getImage("resource/barbearian.png", tower.getAngle());
-        drawTower(rotatedImage, pos, tower.getAngle());
+        drawTower(tower.getPos(), tower.getAngle(), Constant.getInstance().IMAGE_PATH.BARBEARIAN);
     }
 
-    private void drawTower(Image image, Vector pos, double angle) {
+    private void drawTower(Vector notRotatedPos, double angle, String path) {
 
         final double sin = Math.abs(Math.sin(angle));
         final double cos = Math.abs(Math.cos(angle));
 
+        Vector pos = getRealPos(notRotatedPos);
         double width = windowState.getTileSize() * cos + windowState.getTileSize() * sin;
         int offset = (int) ((width - windowState.getTileSize()) / 2);
+        BufferedImage image = ImageHandler.getImage(path, angle);
 
         graphics.drawImage(image, pos.getIntX() - offset, pos.getIntY() - offset, (int) width, (int) width, null);
     }
