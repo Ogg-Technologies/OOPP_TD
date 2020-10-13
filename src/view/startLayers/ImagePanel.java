fd7a99fd435e7991f3d687a00381c3acec117e36
ApansKaffekopp <55796574+ApansKaffekopp@users.noreply.github.com>
2020-10-13 19:31:53 +0200

    startImage adjusts to windowsize instead of distorting

--- src/view/startLayers/ImagePanel.java
+++ src/view/startLayers/ImagePanel.java
@@ -11,22 +11,34 @@ public class ImagePanel extends JPanel {
     private final BufferedImage image;
     private int width;
     private int height;
+    private int x;
+    private int y;
 
     public ImagePanel(Vector size) {
-        this.image =  ImageHandler.getImage("resource/startScreen.png");
+        this.image = ImageHandler.getImage("resource/startScreen.png");
         this.width = size.getIntX();
         this.height = size.getIntY();
+        updateImage();
     }
 
     private void updateImage() {
-        this.width = getWidth();
-        this.height = getHeight();
+        if (getWidth() / 16 > getHeight() / 9) {
+            width = getWidth();
+            height = 9 * getWidth() / 16;
+            y = -(height - getHeight()) / 2;
+            x = 0;
+        } else {
+            width = getHeight() * 16 / 9;
+            height = getHeight();
+            x = -(width - getWidth()) / 2;
+            y = 0;
+        }
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         updateImage();
-        g.drawImage(image, 0, 0, width, height, null);
+        g.drawImage(image, x, y, width, height, null);
     }
 }
