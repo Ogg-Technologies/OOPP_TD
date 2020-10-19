ebe4e9d26949e3a7a6e9e308e478a98b7c40a9c7
Erik <erik.g.eriksson1@gmail.com>
2020-10-19 14:56:44 +0200

    Changed PathIteratorTest

--- test/model/game/enemy/PathIteratorTest.java
+++ test/model/game/enemy/PathIteratorTest.java
@@ -1,6 +1,7 @@
 package model.game.enemy;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import utils.Vector;
 
@@ -17,46 +18,73 @@ class PathIteratorTest {
 
     private PathIterator p;
 
-    @BeforeEach
-    void setUp() {
-        List<Vector> list = new ArrayList<>();
-        list.add(new Vector(1, 1));
-        list.add(new Vector(2, 1));
-        list.add(new Vector(2, 2));
-        list.add(new Vector(2, 3));
-        p = new PathIterator(list);
+    @Nested
+    class StandardConstructor {
+
+        @BeforeEach
+        void setUp() {
+            List<Vector> list = new ArrayList<>();
+            list.add(new Vector(1, 1));
+            list.add(new Vector(2, 1));
+            list.add(new Vector(2, 2));
+            list.add(new Vector(2, 3));
+            p = new PathIterator(list);
+        }
+
+        @Test
+        void canIterateForwards() {
+            assertTrue(p.hasNext());
+            assertEquals(new Vector(1, 1), p.next());
+            assertTrue(p.hasNext());
+            assertEquals(new Vector(2, 1), p.next());
+            assertTrue(p.hasNext());
+            assertEquals(new Vector(2, 2), p.next());
+            assertTrue(p.hasNext());
+            assertEquals(new Vector(2, 3), p.next());
+            assertFalse(p.hasNext());
+            assertThrows(NoSuchElementException.class, () -> p.next());
+        }
+
+        @Test
+        void canStepBackwards() {
+            assertFalse(p.hasPrevious());
+            assertTrue(p.hasNext());
+            assertEquals(new Vector(1, 1), p.next());
+
+            assertFalse(p.hasPrevious());
+            assertTrue(p.hasNext());
+            assertEquals(new Vector(2, 1), p.next());
+
+            assertTrue(p.hasPrevious());
+            assertTrue(p.hasNext());
+            assertEquals(new Vector(1, 1), p.previous());
+
+            assertFalse(p.hasPrevious());
+            assertTrue(p.hasNext());
+            assertThrows(NoSuchElementException.class, () -> p.previous());
+        }
     }
 
-    @Test
-    void canIterateForwards() {
-        assertTrue(p.hasNext());
-        assertEquals(new Vector(1, 1), p.next());
-        assertTrue(p.hasNext());
-        assertEquals(new Vector(2, 1), p.next());
-        assertTrue(p.hasNext());
-        assertEquals(new Vector(2, 2), p.next());
-        assertTrue(p.hasNext());
-        assertEquals(new Vector(2, 3), p.next());
-        assertFalse(p.hasNext());
-        assertThrows(NoSuchElementException.class, () -> p.next());
-    }
+    @Nested
+    class FlyingPath {
 
-    @Test
-    void canStepBackwards() {
-        assertFalse(p.hasPrevious());
-        assertTrue(p.hasNext());
-        assertEquals(new Vector(1, 1), p.next());
+        @BeforeEach
+        void setUp() {
+            List<Vector> list = new ArrayList<>();
+            list.add(new Vector(1, 1));
+            list.add(new Vector(2, 1));
+            list.add(new Vector(2, 2));
+            list.add(new Vector(2, 3));
+            list.add(new Vector(3, 3));
+            p = new PathIterator(list, 3);
+        }
 
-        assertFalse(p.hasPrevious());
-        assertTrue(p.hasNext());
-        assertEquals(new Vector(2, 1), p.next());
-
-        assertTrue(p.hasPrevious());
-        assertTrue(p.hasNext());
-        assertEquals(new Vector(1, 1), p.previous());
-
-        assertFalse(p.hasPrevious());
-        assertTrue(p.hasNext());
-        assertThrows(NoSuchElementException.class, () -> p.previous());
+        @Test
+        void flyingPathIsCorrect() {
+            assertEquals(new Vector(1, 1), p.next());
+            assertEquals(new Vector(2, 3), p.next());
+            assertEquals(new Vector(3, 3), p.next());
+            assertThrows(NoSuchElementException.class, () -> p.next());
+        }
     }
 }
