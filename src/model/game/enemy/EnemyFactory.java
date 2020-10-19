f9d7c2c1ce04507aa27764add41cc89ac7140d87
Erik <erik.g.eriksson1@gmail.com>
2020-10-19 13:15:09 +0200

    Moved flying path algorithm to PathIterator

--- src/model/game/enemy/EnemyFactory.java
+++ src/model/game/enemy/EnemyFactory.java
@@ -5,10 +5,7 @@ import model.game.enemy.concreteenemies.BasicEnemy;
 import model.game.enemy.concreteenemies.FlyingFish;
 import utils.Vector;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 /**
  * @author Oskar, Behroz, Samuel, Erik
@@ -53,23 +50,6 @@ public class EnemyFactory {
     }
 
     public Enemy createFlyingFish() {
-        return new FlyingFish(baseDamager, new PathIterator(getFlyingPath(this.path, Config.INSTANCE.FLYING_FISH.FLY_AMOUNT)));
-    }
-
-
-    /**
-     * Filters the given basePath to only take the nth element from it (flyAmount) and returns it as a new list.
-     * Used to make path iterators for flying fish where they skip over most tiles
-     *
-     * @param basePath  The path containing all the path tile positions
-     * @param flyAmount The number of tiles the new path will "fly over" between each element in the path
-     * @return A new list containing only the flyAmount'th element from the basePath
-     */
-    private List<? extends Vector> getFlyingPath(List<? extends Vector> basePath, int flyAmount) {
-        List<Vector> flyPath = new ArrayList<>(basePath);
-        return IntStream.range(0, flyPath.size())
-                .filter(n -> n % flyAmount == 0)
-                .mapToObj(flyPath::get)
-                .collect(Collectors.toList());
+        return new FlyingFish(baseDamager, new PathIterator(this.path, Config.INSTANCE.FLYING_FISH.FLY_AMOUNT));
     }
 }
--- src/model/game/enemy/PathIterator.java
+++ src/model/game/enemy/PathIterator.java
@@ -2,8 +2,11 @@ package model.game.enemy;
 
 import utils.Vector;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * @author Oskar, Erik
@@ -15,9 +18,46 @@ public class PathIterator {
     private final List<? extends Vector> path;
     private int current;
 
+    /**
+     * Creates a PathIterator from the given path
+     *
+     * @param path List of Vectors of the whole path
+     */
     public PathIterator(List<? extends Vector> path) {
         this.path = path;
         current = -1;
+    }
+
+    /**
+     * Creates a PathIterator where only the keepNth'th elements are kept from the path argument
+     * Used to be able to skip over parts of the path (like flying enemies)
+     *
+     * @param path    List of Vectors of the whole path
+     * @param keepNth Integer determining which nth elements are kept, while the other elements are ignored. Must be > 0
+     */
+    public PathIterator(List<? extends Vector> path, int keepNth) {
+        if (keepNth <= 0) {
+            throw new IllegalArgumentException("keepNth cannot be zero or below");
+        }
+
+        this.path = calculateFlyingPath(path, keepNth);
+        current = -1;
+    }
+
+    /**
+     * Filters the given basePath to only take the nth element from it (flyAmount) and returns it as a new list.
+     * Used to make path iterators for flying fish where they skip over most tiles
+     *
+     * @param basePath  The path containing all the path tile positions
+     * @param flyAmount The number of tiles the new path will "fly over" between each element in the path
+     * @return A new list containing only the flyAmount'th element from the basePath
+     */
+    private List<? extends Vector> calculateFlyingPath(List<? extends Vector> basePath, int flyAmount) {
+        List<Vector> flyPath = new ArrayList<>(basePath);
+        return IntStream.range(0, flyPath.size())
+                .filter(n -> n % flyAmount == 0)
+                .mapToObj(flyPath::get)
+                .collect(Collectors.toList());
     }
 
     public boolean hasNext() {
