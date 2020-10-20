5fb309f206470571e5a4266ac20cead4c5187b79
Oskar <ogg.developer@gmail.com>
2020-10-20 16:51:45 +0200

    Rounds all angles of images to improve performance

--- src/main/java/view/texture/ImageInfo.java
+++ src/main/java/view/texture/ImageInfo.java
@@ -5,8 +5,14 @@ import java.util.Objects;
 /**
  * @author Oskar, Samuel
  * Saves a path and an angle together, to check if the correlation exists in imageHandler
+ * Rounds angle to a nearby number to avoid too much precision in angles
  */
 public class ImageInfo {
+    /**
+     * Divides the 2*PI of a circle into this many discrete angles. All angles get rounded to the closest one.
+     * A high value here means more precision in rotated images. A lower value means better performance.
+     */
+    public static final int NUMBER_OF_POSSIBLE_ANGLES = 100;
     public final String path;
     public final double angle;
 
@@ -15,7 +21,24 @@ public class ImageInfo {
             throw new IllegalArgumentException("path may not be null");
         }
         this.path = path;
-        this.angle = angle;
+        this.angle = adjustAngle(angle);
+    }
+
+    /**
+     * First removes any extra rotations in the angle.
+     * Then it rounds the angle to the closest one of the evenly distributed NUMBER_OF_POSSIBLE_ANGLES.
+     *
+     * @param angle the angle to round
+     * @return the rounded angle
+     */
+    private double adjustAngle(double angle) {
+        double circleFraction = angle / (Math.PI * 2);
+        circleFraction = circleFraction % 1;
+
+        double roundedCircleFraction = Math.round(circleFraction * NUMBER_OF_POSSIBLE_ANGLES) / (double) NUMBER_OF_POSSIBLE_ANGLES;
+        double newAngle = roundedCircleFraction * (Math.PI * 2);
+
+        return newAngle;
     }
 
     @Override
